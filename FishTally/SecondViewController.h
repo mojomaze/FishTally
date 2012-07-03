826289f0aefb5342e4d2d9532f5bf4ad075ccb12
mwinkler <mhwinkler@gmail.com>
2012-07-03 08:50:34 -0500

    Initial Commit

---
+++ FishTally/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  FishTally
+//
+//  Created by Mark Winkler on 7/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
